ac7a30f95d815f35c69feb94163978d18685bb36
Kim-jiseong <kimjs030727@gmail.com>
2025-11-04 22:19:08 +0900

    *중간 업로드

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,29 +1,39 @@
 package com.mysite.sbb;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-//import java.util.List; // Test02
-import java.util.Optional; // Test03
-
-//import java.time.LocalDateTime; // Test01
+import static org.junit.jupiter.api.Assertions.assertEquals;  //use assertEquals
+import static org.junit.jupiter.api.Assertions.assertTrue;    //use assertTrue
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+//To use below Types
+//import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 class SbbApplicationTests {
 
 	@Autowired
 	private QuestionRepository questionRepository;
+	@Autowired
+	//private AnswerRepository answerRepository;
 	
+	@Transactional
 	@Test
 	void testJpa() {
-		Optional<Question> oq = this.questionRepository.findById(1);
-		if(oq.isPresent()) {
-			Question q = oq.get();
-			assertEquals("What is Sbb?", q.getSubject());
-		}
+		// Test 11 - confirm Question's Answer
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		
+		List<Answer> answerList = q.getAnswerList();
+		
+		assertEquals(1, answerList.size());
+		assertEquals("Yes. It Generates Automatically.", answerList.get(0).getContent());
+		
 		
 		/* Test 01 - upload
 		Question q1 = new Question();
@@ -45,6 +55,67 @@ class SbbApplicationTests {
 		
 		Question q = all.get(0);
 		assertEquals("What is Sbb?", q.getSubject());
+		*/
+		
+		/* Test 03 - findAll, find Id
+				Optional<Question> oq = this.questionRepository.findById(1);
+				if(oq.isPresent()) {
+					Question q = oq.get();
+					assertEquals("What is Sbb?", q.getSubject());
+				}
+		*/
+				
+		/* Test 04 - find Subject
+		Question q = this.questionRepository.findBySubject("What is Sbb?");
+		assertEquals(1, q.getId());
+		*/
+		
+		/* Test 05 - find Subject and Content
+		Question q = this.questionRepository.findBySubjectAndContent("What is Sbb?", "I want to learn about Sbb");
+		assertEquals(1, q.getId());
+		*/
+		
+		/* Test 06 - find Subject(Like)
+				List<Question> qList = this.questionRepository.findBySubjectLike("Sbb%");
+				Question q = qList.get(0);
+				assertEquals("What is Sbb?", q.getSubject());
+		*/
+		
+		/* Test 07 - modify subject
+				Optional<Question> oq = this.questionRepository.findById(1);
+				assertTrue(oq.isPresent());
+				Question q = oq.get();
+				q.setSubject("modified Subject");
+				
+				this.questionRepository.save(q);
+		*/
+		
+		/* Test 08 - delect
+				assertEquals(2, this.questionRepository.count());
+				Optional<Question> oq = this.questionRepository.findById(1);
+				assertTrue(oq.isPresent());
+				Question q = oq.get();
+				this.questionRepository.delete(q);
+				assertEquals(1, this.questionRepository.count());
+		*/
+		
+		/* Test 09 - Add Answer
+				Optional<Question> oq = this.questionRepository.findById(2);
+				assertTrue(oq.isPresent());
+				Question q = oq.get();
+				
+				Answer a = new Answer();
+				a.setContent("Yes. It Generates Automatically.");
+				a.setQuestion(q);
+				a.setCreateDate(LocalDateTime.now());
+				this.answerRepository.save(a);
+		*/
+		
+		/* Test 10 - confirm Answer's question
+				Optional<Answer> oa = this.answerRepository.findById(1);
+				assertTrue(oa.isPresent());
+				Answer a = oa.get();
+				assertEquals(2, a.getQuestion().getId());
 		*/
 	}
 
